9882769260741634a8d5be6e1f3262cc0d42d5ef
kevin <liwengousuzhou@gmail.com>
2023-01-29 16:50:29 +0800

    change port

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
-
+//
     const response = await fetch('https://seep-ai.onrender.com', {
         method: 'POST',
         headers: {
